8264a04f915e12f2ce102f104b5e154eb09a6c43
Ramu Pasupuleti <126960469+ramupasupuleti011@users.noreply.github.com>
2026-03-19 18:58:07 +0530

    Enhance jenkins.sh for installation and service setup
    Updated the Jenkins installation script to include system updates, Java installation, and service management commands.

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,29 @@
+#!/bin/bash
 
-#! /bin/bash
+# Update system
+sudo yum update -y
+
+# Install Java 17
+sudo yum install java-17-amazon-corretto -y
+
+# Add Jenkins repository
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import Jenkins key
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-# Add required dependencies for the jenkins package
-yum install java-17-amazon-corretto jenkins -y
+
+# Install Jenkins
 sudo yum install jenkins -y
-sudo systemctl restart jenkins
+
+# Reload systemd
 sudo systemctl daemon-reload
+
+# Enable Jenkins at boot
+sudo systemctl enable jenkins
+
+# Start Jenkins
+sudo systemctl start jenkins
+
+# Check Jenkins status
+sudo systemctl status jenkins
